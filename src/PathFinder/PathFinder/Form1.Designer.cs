e836939f9c375ec8806b4872745a6efba8c6afbf
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-04-06 12:54:42 +0700

    interface

--- src/PathFinder/PathFinder/Form1.Designer.cs
+++ src/PathFinder/PathFinder/Form1.Designer.cs
@@ -28,20 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.radioButtonUCS = new System.Windows.Forms.RadioButton();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFileName.Location = new System.Drawing.Point(61, 120);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(188, 29);
+            this.textBoxFileName.TabIndex = 0;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelFileName.Location = new System.Drawing.Point(53, 83);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(111, 29);
+            this.labelFileName.TabIndex = 1;
+            this.labelFileName.Text = "Filename:";
+            // 
+            // radioButtonUCS
+            // 
+            this.radioButtonUCS.AutoSize = true;
+            this.radioButtonUCS.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButtonUCS.Location = new System.Drawing.Point(65, 263);
+            this.radioButtonUCS.Name = "radioButtonUCS";
+            this.radioButtonUCS.Size = new System.Drawing.Size(66, 26);
+            this.radioButtonUCS.TabIndex = 2;
+            this.radioButtonUCS.TabStop = true;
+            this.radioButtonUCS.Text = "UCS";
+            this.radioButtonUCS.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButtonA.Location = new System.Drawing.Point(65, 295);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(55, 26);
+            this.radioButtonA.TabIndex = 3;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "A*";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(61, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Method:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(61, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "VISUALIZE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelStart.Location = new System.Drawing.Point(65, 372);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(128, 29);
+            this.labelStart.TabIndex = 6;
+            this.labelStart.Text = "Start point:";
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelEnd.Location = new System.Drawing.Point(65, 413);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(116, 29);
+            this.labelEnd.TabIndex = 7;
+            this.labelEnd.Text = "End point:";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSearch.Location = new System.Drawing.Point(69, 458);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(112, 34);
+            this.buttonSearch.TabIndex = 8;
+            this.buttonSearch.Text = "SEARCH";
+            this.buttonSearch.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(950, 544);
-            Margin = new Padding(3, 2, 3, 2);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1059, 567);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.radioButtonUCS);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.textBoxFileName);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBoxFileName;
+        private Label labelFileName;
+        private RadioButton radioButtonUCS;
+        private RadioButton radioButtonA;
+        private Label label1;
+        private Button button1;
+        private Label labelStart;
+        private Label labelEnd;
+        private Button buttonSearch;
     }
 }
