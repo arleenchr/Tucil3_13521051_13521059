f8f41d5dcc412436f432322dba775fe0669955f5
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-04-11 01:23:31 +0700

    interface

--- src/PathFinder/PathFinder.Designer.cs
+++ src/PathFinder/PathFinder.Designer.cs
@@ -32,6 +32,16 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.labelPathFinder = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.labelMethod = new System.Windows.Forms.Label();
+            this.labelUCS = new System.Windows.Forms.RadioButton();
+            this.buttonA = new System.Windows.Forms.RadioButton();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,32 +51,154 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(642, 101);
+            this.chart1.Location = new System.Drawing.Point(371, 122);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.Size = new System.Drawing.Size(498, 403);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // labelPathFinder
+            // 
+            this.labelPathFinder.AutoSize = true;
+            this.labelPathFinder.Font = new System.Drawing.Font("Candara", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPathFinder.Location = new System.Drawing.Point(394, 24);
+            this.labelPathFinder.Name = "labelPathFinder";
+            this.labelPathFinder.Size = new System.Drawing.Size(252, 49);
+            this.labelPathFinder.TabIndex = 1;
+            this.labelPathFinder.Text = "PATH FINDER";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFileName.Location = new System.Drawing.Point(60, 155);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(187, 32);
+            this.textBoxFileName.TabIndex = 2;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFileName.Location = new System.Drawing.Point(56, 122);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(111, 29);
+            this.labelFileName.TabIndex = 3;
+            this.labelFileName.Text = "Filename:";
+            // 
+            // labelMethod
+            // 
+            this.labelMethod.AutoSize = true;
+            this.labelMethod.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMethod.Location = new System.Drawing.Point(56, 224);
+            this.labelMethod.Name = "labelMethod";
+            this.labelMethod.Size = new System.Drawing.Size(99, 29);
+            this.labelMethod.TabIndex = 4;
+            this.labelMethod.Text = "Method:";
+            // 
+            // labelUCS
+            // 
+            this.labelUCS.AutoSize = true;
+            this.labelUCS.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUCS.Location = new System.Drawing.Point(61, 260);
+            this.labelUCS.Name = "labelUCS";
+            this.labelUCS.Size = new System.Drawing.Size(70, 28);
+            this.labelUCS.TabIndex = 5;
+            this.labelUCS.TabStop = true;
+            this.labelUCS.Text = "UCS";
+            this.labelUCS.UseVisualStyleBackColor = true;
+            // 
+            // buttonA
+            // 
+            this.buttonA.AutoSize = true;
+            this.buttonA.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonA.Location = new System.Drawing.Point(61, 294);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(57, 28);
+            this.buttonA.TabIndex = 6;
+            this.buttonA.TabStop = true;
+            this.buttonA.Text = "A*";
+            this.buttonA.UseVisualStyleBackColor = true;
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStart.Location = new System.Drawing.Point(56, 367);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(128, 29);
+            this.labelStart.TabIndex = 7;
+            this.labelStart.Text = "Start point:";
+            // 
+            // labelEnd
+            // 
+            this.labelEnd.AutoSize = true;
+            this.labelEnd.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEnd.Location = new System.Drawing.Point(56, 457);
+            this.labelEnd.Name = "labelEnd";
+            this.labelEnd.Size = new System.Drawing.Size(116, 29);
+            this.labelEnd.TabIndex = 8;
+            this.labelEnd.Text = "End point:";
+            // 
+            // textBoxStart
+            // 
+            this.textBoxStart.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxStart.Location = new System.Drawing.Point(61, 401);
+            this.textBoxStart.Name = "textBoxStart";
+            this.textBoxStart.Size = new System.Drawing.Size(187, 32);
+            this.textBoxStart.TabIndex = 9;
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEnd.Location = new System.Drawing.Point(60, 489);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(187, 32);
+            this.textBoxEnd.TabIndex = 10;
             // 
             // PathFinder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1077, 559);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.textBoxStart);
+            this.Controls.Add(this.labelEnd);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.labelUCS);
+            this.Controls.Add(this.labelMethod);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.labelPathFinder);
             this.Controls.Add(this.chart1);
             this.Name = "PathFinder";
-            this.Text = "chart";
+            this.Text = "Path Finder";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label labelPathFinder;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Label labelMethod;
+        private System.Windows.Forms.RadioButton labelUCS;
+        private System.Windows.Forms.RadioButton buttonA;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.TextBox textBoxEnd;
     }
 }
 
